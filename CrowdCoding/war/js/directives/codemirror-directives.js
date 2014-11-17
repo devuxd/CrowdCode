268ff6625e7cb00c1ff36cdfd4e230a890c49411
fraibcicoi <fabio1.ricci@mail.polimi.it>
2014-11-17 14:16:23 -0800

    bug fixing

--- CrowdCoding/war/js/directives/codemirror-directives.js
+++ CrowdCoding/war/js/directives/codemirror-directives.js
@@ -1,21 +1,24 @@
 
-myApp.directive('codeMirrorReadonly', function($compile) {
+myApp.directive('codeMirrorReadonly',function($compile,functionsService) {
     return {
         restrict: 'EA',
 
-        template:'<div ng-model="code" ui-codemirror="{ onLoad : codemirrorLoaded }"></div>',
-
+        template:'<div ui-codemirror="{ onLoad : codemirrorLoaded }"></div>',
         scope: {
             code: "="
         },
           controller: function($scope,$element){
-        	$scope.codemirrorLoaded = function(codeMirror){
+           	$scope.codemirrorLoaded = function(codeMirror){
 
         		codeMirror.setOption("readOnly", "true");
         		codeMirror.setOption("theme", "custom");
         		codeMirror.setOption("tabindex", "-1");
         		codeMirror.setSize(null,'auto');
-           	}
+                //Changed from  //ng-model="code" to  codeMirror.setValue($scope.code); to do the hilighting
+
+                codeMirror.setValue($scope.code);
+                functionsService.highlightPseudoSegments(codeMirror,[],false);
+           	};
         }
-    }
+    };
 });
