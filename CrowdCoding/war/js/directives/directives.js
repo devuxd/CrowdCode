e74d7a486c17e0d7c71b4cc40ba2732eaba1ff1b
woz <wozmind@gmail.com>
2015-02-21 12:39:58 -0800

    - ui tweaks for at list - hidden example for write test cases

--- CrowdCoding/war/js/directives/directives.js
+++ CrowdCoding/war/js/directives/directives.js
@@ -680,10 +680,18 @@ angular
     return {
         restrict: "EA",
         scope: true,
-        templateUrl: '/html/templates/adt_list.html',
+        templateUrl: '/html/templates/ui/adt_list.html',
         link: function($scope, $element, $attributes) {
             $scope.ADTs = ADTService.getAllADTs();
             $scope.ADTs.selectedADT = -1;
+            $scope.buildStructure = function(adt){
+                var struct = '{';
+                angular.forEach(adt.structure,function(field){
+                    struct += '\n  '+field.name+': '+field.type;
+                })
+                struct += '\n}';
+                return struct;
+            };
         }
     }
 }]);
