8bc9b2f39ebaadd1184c612d3e2ea4e03531fbde
woz <wozmind@gmail.com>
2015-02-26 15:54:47 -0800

    fixed son diff reader

--- CrowdCoding/war/js/directives/ace-editor-directives.js
+++ CrowdCoding/war/js/directives/ace-editor-directives.js
@@ -146,79 +146,127 @@ angular
             var unwatch = scope.$watch('old+new',function(){
                 if( scope.old != undefined && scope.new != undefined){
                     
-                    // if the passed values are strings, add quotes
-                    // otherwise parse them to retrieve the JS object
+                    
                     var oldObj,newObj;
-                    try {
-                        oldObj = JSON.parse( scope.old );
-                        newObj = JSON.parse( scope.new );
-                    } catch(e) {
-                        console.log(e);
-                        oldObj = '"'+scope.old+'"';
-                        newObj = '"'+scope.new+'"';
-                    }
+
+                    // try to parse the old and new value to a JSON object
+                    // if the conversion fails,  simply add quotes
+                    oldObj = safeJsonParse( scope.old );
+                    newObj = safeJsonParse( scope.new );
 
                     // initialize the diff result
                     var diffHtml = '';
 
+                    // if the type is different
+                    if( oldObj == null || newObj == null || oldObj.constructor != newObj.constructor ){
+
+                        if( typeof(oldObj) == 'object' )
+                            diffHtml += joinLines( angular.toJson(oldObj, true) , 'line removed', 0);
+                        else 
+                            diffHtml += joinLines( oldObj.toString(), 'line removed', 0);
+
+
+                        if( typeof(newObj) == 'object' )
+                            diffHtml += joinLines( angular.toJson(newObj, true) , 'line added', 0);
+                        else if ( typeof(newObj) == 'string' )
+                            diffHtml += joinLines( '"'+newObj+'"', 'line added', 0);     
+                        else
+                            diffHtml += joinLines( newObj.toString(), 'line added', 0);                    
+
+                        scope.diffHtml = diffHtml;
+                    }
                     // if the type of new is an object/array
-                    if( typeof(newObj) == 'object' ){
-                        // find the differences in the 
-                        // first layer of properties
-                        var compareResults = [];
-                        for( var field in newObj ){
-                            compareResults[ field ] = deepCompare( oldObj[field], newObj[field] );
+                    else {
+
+                        var oldFields = Object.keys(oldObj);
+                        var newFields = Object.keys(newObj);
+
+                        var sharedFields  = oldFields.filter(function(value){ return newFields.indexOf(value) != -1; });
+                        var removedFields = oldFields.filter(function(value){ return newFields.indexOf(value) == -1; });
+                        var addedFields   = newFields.filter(function(value){ return oldFields.indexOf(value) == -1; });
+
+                        var isArray = newObj.constructor == Array;
+
+                        for( var f = 0 ; f < removedFields.length ; f++ ){
+                            var name = removedFields[f];
+                            var text = angular.toJson( oldObj[name], true) + ',';
+                            if( !isArray ) text = '"'+name+'" : ' + text;
+                            diffHtml += joinLines( text, 'line removed', 2) ;
+                            diffHtml += '\n';
                         }
 
-                        // for each property of the first layer  
-                        var fields = Object.keys(compareResults);
-                        for( var k = 0; k < fields.length; k++ ){
-                            var field = fields[k];
+                        for( var f = 0 ; f < sharedFields.length ; f++ ){
+                            var name = sharedFields[f];
+
+                            var text = angular.toJson( oldObj[name], true) + ',';
+                            if( !isArray ) text = '"'+name+'" : ' + text;
+                            diffHtml += joinLines( text, 'line removed', 2) ;
+
+
+                            var text = angular.toJson( newObj[name], true) + ',';
+                            if( !isArray ) text = '"'+name+'" : ' + text;
+                            diffHtml += joinLines( text, 'line added', 2) ;
+
+                            diffHtml += '\n';
+                        }
+
+                        for( var f = 0 ; f < addedFields.length ; f++ ){
+                            var name = addedFields[f];
+                            var text = angular.toJson( newObj[name], true) + ',';
+                            if( !isArray ) text = '"'+name+'" : ' + text;
+                            diffHtml += joinLines( text, 'line added', 2) ;
+
+                            diffHtml += '\n';
+                        }
+
+                        // // find the differences in the 
+                        // // first layer of properties
+                        // var compareResults = [];
+                        // for( var field in newObj ){
+                        //     compareResults[ field ] = deepCompare( oldObj[field], newObj[field] );
+                        // }
+
+                        // // for each property of the first layer  
+                        // var fields = Object.keys(compareResults);
+                        // for( var k = 0; k < fields.length; k++ ){
+                        //     var field = fields[k];
                             
-                            // if the fields are equal 
-                            // concat the value as is in the 
-                            // diffHtml val
-                            if( compareResults[field] ) {
-                                var text = angular.toJson( newObj[field] !== undefined ? newObj[field] : '', true);
-                                if( newObj.constructor == Object ) text = '"'+field+'" : ' + text;
-                                if( k != fields.length -1 ) text += ',';
+                        //     // if the fields are equal 
+                        //     // concat the value as is in the 
+                        //     // diffHtml val
+                        //     if( compareResults[field] ) {
+                        //         var text = angular.toJson( newObj[field] !== undefined ? newObj[field] : '', true);
+                        //         if( newObj.constructor == Object ) text = '"'+field+'" : ' + text;
+                        //         if( k != fields.length -1 ) text += ',';
 
-                                diffHtml += joinLines( text, 'line ', 2) ;
-                            } 
-                            // otherwise first add the old value of the property as 'removed'
-                            // and after add the new value as 'added'
-                            else {
-                                // when the field is not defined in the oldObj
-                                // show just the new value
-                                if( oldObj[field] !== undefined ){
-                                    var removedText = angular.toJson( oldObj[field], true);
-                                    if( newObj.constructor == Object )
-                                        removedText = '"'+field+'" : ' + removedText;
-                                    diffHtml += joinLines( removedText, 'line removed', 2) ;
-                                }
+                        //         diffHtml += joinLines( text, 'line ', 2) ;
+                        //     } 
+                        //     // otherwise first add the old value of the property as 'removed'
+                        //     // and after add the new value as 'added'
+                        //     else {
+                        //         // when the field is not defined in the oldObj
+                        //         // show just the new value
+                        //         if( oldObj[field] !== undefined ){
+                        //             var removedText = angular.toJson( oldObj[field], true);
+                        //             if( newObj.constructor == Object )
+                        //                 removedText = '"'+field+'" : ' + removedText;
+                        //             diffHtml += joinLines( removedText, 'line removed', 2) ;
+                        //         }
 
-                                var addedText   = angular.toJson( newObj[field], true);
-                                if( k != fields.length -1 ) addedText += ',';
-                                if( newObj.constructor == Object ){
-                                    addedText   = '"'+field+'" : ' + addedText;
-                                }
-                                diffHtml += joinLines( addedText, 'line added', 2);
+                        //         var addedText   = angular.toJson( newObj[field], true);
+                        //         if( k != fields.length -1 ) addedText += ',';
+                        //         if( newObj.constructor == Object ){
+                        //             addedText   = '"'+field+'" : ' + addedText;
+                        //         }
+                        //         diffHtml += joinLines( addedText, 'line added', 2);
                                     
-                            }
-                            if( k != fields.length -1 ) diffHtml += '\n';
-                        }
+                        //     }
+                        //     if( k != fields.length -1 ) diffHtml += '\n';
+                        // }
 
                         // pick the appropriate set of brackets for the final diff result
                         if( newObj.constructor == Array )  scope.diffHtml = '[\n'+diffHtml+']';
                         if( newObj.constructor == Object ) scope.diffHtml = '{\n'+diffHtml+'}';
-                    } 
-                    // if the type of newObj is not an object, it is a String, Number or Boolean
-                    else {
-
-                        diffHtml += joinLines( oldObj.toString(), 'line removed', 0);
-                        diffHtml += joinLines( newObj.toString(), 'line added', 0);
-
-                        scope.diffHtml = diffHtml;
                     }
                     
 
@@ -301,6 +349,31 @@ angular
     };
 });
 
+/** 
+ safe json parse 
+**/
+function safeJsonParse(json){
+    var obj = null;
+    if( json == 'Infinity' )
+        obj = Infinity;
+    else if( json == 'undefined' )
+        obj = undefined;
+    else if( json == 'NaN' )
+        obj = NaN;
+    else if( json == 'null' )
+        obj = null;
+    else {
+        try {
+            obj = JSON.parse(json);
+        } catch( e ){
+            obj = '"'+json+'"';
+        }
+    }
+
+    return obj;
+}
+
+
 /**
   search and highlight the json 
 **/
@@ -330,7 +403,6 @@ function jsonSyntaxHighlight(json) {
   tags preeceding the content with #identation spaces
 */
 function joinLines(text,cssClass,identation){
-    console.log('text is '+text);
     var lines = text.split('\n');
     var html = '';
     for( var li in lines ){
