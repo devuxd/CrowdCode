74705a69052986b45674c0c5796f5ccbdf997272
woz <wozmind@gmail.com>
2015-03-04 13:09:04 -0800

    premerge

--- CrowdCoding/war/js/directives/testResult.js
+++ CrowdCoding/war/js/directives/testResult.js
@@ -5,10 +5,10 @@ angular
 function testResult() {
     return {
         restrict: 'E',
-        scope: { test: '='},
+        scope: { test: '=', 'funct'},
         templateUrl: '/html/templates/ui/test_result.html',
         link: function(scope,element,attributes){
-            
+
         	scope.diffMode = true;
         	scope.switchMode = function(){
         		scope.diffMode = !scope.diffMode;
