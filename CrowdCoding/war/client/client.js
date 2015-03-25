3276e4e4d2701b16dd6c72b8c04462b7a3787d37
woz <wozmind@gmail.com>
2015-03-25 16:29:59 -0700

    removed main controller, main logic is now in crowdCodeApp.run() fixes to admin

--- CrowdCoding/war/client/client.js
+++ CrowdCoding/war/client/client.js
@@ -24,6 +24,94 @@ angular
     .constant('projectId'  ,projectId)
 	.constant('firebaseUrl',firebaseURL)
 	.constant('logoutUrl'  ,logoutURL)
-	.run();
+	.run(function($rootScope, $interval, $modal, $firebase, firebaseUrl, logoutUrl, userService,  functionsService, ADTService, avatarFactory){
+
+		// current session variables
+		$rootScope.projectId    = projectId;
+		$rootScope.workerId     = workerId;
+		$rootScope.workerHandle = workerHandle;
+		$rootScope.firebaseURL  = firebaseURL;
+		$rootScope.userData     = userService.data;
+		$rootScope.logoutUrl    = logoutUrl;
+		$rootScope.avatar       = avatarFactory.get;
+		
+		var profileModal            = $modal({scope: $rootScope, container: 'body', animation: 'am-fade-and-scale', placement: 'center', template: '/client/widgets/popup_user_profile.html', show: false});
+		var servicesLoadingStatus   = {};
+		var loadingServicesInterval = $interval(loadServices(), 200);
+
+		$rootScope.$on('showProfileModal', showProfileModal);
+		$rootScope.$on('serviceLoaded'   , serviceLoaded);
+		$rootScope.$on('sendFeedback', sendFeedback);
+
+	
+		$rootScope.makeDirty = makeFormDirty;
+		
+
+		function loadServices(){
+			servicesLoadingStatus = {};
+			functionsService.init();
+			ADTService.init();
+		}
+
+		function serviceLoaded(event,nameOfTheService){
+			servicesLoadingStatus[nameOfTheService] = true;
+
+			if ( servicesLoadingStatus.hasOwnProperty('functions') && servicesLoadingStatus.hasOwnProperty('adts') ) {
+
+				$interval.cancel(loadingServicesInterval);
+				loadingServicesInterval = undefined;
+
+
+				userService.listenForJobs();
+				userService.listenForLogoutWorker();
+
+
+				$rootScope.$broadcast('loadMicrotask');
+
+				$rootScope.$broadcast('run-tutorial','main', false, function(){
+					$rootScope.$broadcast('showProfileModal');
+				});
+			}
+		}
+
+		function showProfileModal() {
+			profileModal.$promise.then(profileModal.show);
+		}
+
+
+		function makeFormDirty(form){
+			angular.forEach(form, function(formElement, fieldName) {
+				// If the fieldname doesn't start with a '$' sign, it means it's form
+				if (fieldName[0] !== '$'){
+					if(angular.isFunction(formElement.$setDirty))
+		                formElement.$setDirty();
+
+					//if formElement as the proprety $addControl means that have other form inside him
+					if (formElement !== undefined && formElement.$addControl)
+						makeFormDirty(formElement);
+				}
+			});
+		}
+
+		function sendFeedback(event, message) {
+			////console.log("message " + message.toString());
+			var feedback = {
+				// 'microtaskType': $scope.microtask.type,
+				// 'microtaskID': $scope.microtask.id,
+				'workerHandle': $rootScope.workerHandle,
+				'workerID': $rootScope.workerId,
+				'feedback': message.toString()
+			};
+
+
+			var feedbackRef = $firebase(new Firebase(firebaseURL + '/feedback'));
+
+			feedbacks = feedbackRef.$asArray();
+			feedbacks.$loaded().then(function() {
+				feedbacks.$add(feedback);
+			});
+
+		}
+	});
 
 
