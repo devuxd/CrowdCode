789ecac80d24ce0df19fcd386a73f9eef20bba98
Meysam Abolghassemi <meysamabl@gmail.com>
2017-06-19 23:51:14 -0400

    add regex for project routing, enable caseSensitive for routing

--- CrowdCode/server/routes/app-routing.js
+++ CrowdCode/server/routes/app-routing.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var path = require('path');
-var router = express.Router();
+var router = express.Router({
+  caseSensitive: true
+});
 var firebase = require('../util/firebase_service');
 const publicDir = path.join(__dirname, '..', '..', 'public');
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.sendFile('welcome.html', {root: publicDir} );
+  res.sendFile('welcome.html', {
+    root: publicDir
+  });
 
 });
 router.get('/clientRequest', function(req, res) {
-    res.sendFile('clientReq/client_request.html', {root: publicDir} );
+  res.sendFile('clientReq/client_request.html', {
+    root: publicDir
+  });
 
 });
-router.get('^/:projectname([a-z]*)', function(req, res) {
-    res.sendFile('clientDist/client.html', {root: publicDir} );
+router.get('/:projectname([a-zA-Z0-9]{3,})', function(req, res) {
+  const projectName = req.params.projectname;
+  console.log("Project Name : " + projectName + "------------------------------");
+  res.sendFile('clientDist/client.html', {
+    root: publicDir
+  });
 
 });
 
