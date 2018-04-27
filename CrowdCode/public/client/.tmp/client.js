ac94ab95adeac7b530b8dcbdf871c1e7556afb6a
Emad Aghayi <emad.aghayi@gmail.com>
2018-04-27 14:11:26 -0400

    update project outline
    update right widget to show the 3rd party API. it is seperated by field from DB

--- CrowdCode/public/client/.tmp/client.js
+++ CrowdCode/public/client/.tmp/client.js
@@ -12707,7 +12707,7 @@ angular.module("widgets/project_outline.template.html", []).run(["$templateCache
     "		</div>\n" +
     "	</div>\n" +
     "	<div style=\"text-align: center\"><strong> Functions: </strong></div>\n" +
-    "	<div ng-repeat=\"f in functions\" class=\"functions\">\n" +
+    "	<div ng-repeat=\"f in functions\" ng-show=\"!f.isThirdPartyAPI\" class=\"functions\">\n" +
     "		<div bs-collapse-toggle class=\"toggler\" > API: {{f.name}}</div>\n" +
     "		<div bs-collapse-target class=\"toggled\">\n" +
     "			<div ng-bind=\"f.description\"></div>\n" +
@@ -12723,6 +12723,22 @@ angular.module("widgets/project_outline.template.html", []).run(["$templateCache
     "		</div>\n" +
     "	</div>\n" +
     "\n" +
+    "	<div style=\"text-align: center\"><strong> 3rd Party API: </strong></div>\n" +
+    "	<div ng-repeat=\"f in functions\" ng-show=\"f.isThirdPartyAPI\" class=\"functions\">\n" +
+    "		<div bs-collapse-toggle class=\"toggler\" > API: {{f.name}}</div>\n" +
+    "		<div bs-collapse-target class=\"toggled\">\n" +
+    "			<div ng-bind=\"f.description\"></div>\n" +
+    "			<div><strong> Parameters </strong></div>\n" +
+    "			<div ng-repeat=\"p in f.parameters\">\n" +
+    "				<span ng-bind=\"p.name\"></span>\n" +
+    "				<span ng-bind=\"p.type\"></span>\n" +
+    "			</div>\n" +
+    "			<div >\n" +
+    "				<strong>Return:</strong>\n" +
+    "				<span ng-bind=\"f.returnType\"></span>\n" +
+    "			</div>\n" +
+    "		</div>\n" +
+    "	</div>\n" +
     "	<!--<div ng-repeat=\"f in thirdPartyAPIs\" class=\"functions\">-->\n" +
     "		<!--<div bs-collapse-toggle class=\"toggler\" > Third party API: {{f.name}}</div>-->\n" +
     "		<!--<div bs-collapse-target class=\"toggled\">-->\n" +
