2b5542d55bcf0240553a81dcfb73f16b7e511a7a
Emad Aghayi <emad.aghayi@gmail.com>
2018-07-20 18:37:00 -0400

    change welcome page

--- public/client/.tmp/client.js
+++ public/client/.tmp/client.js
@@ -853,9 +853,9 @@ angular
                 $rootScope.$broadcast('openDashboard');
                 //$rootScope.$broadcast('fetchMicrotask');
 
-                $rootScope.$broadcast('queue-tutorial', 'main', false, function () {
-                    // $rootScope.$broadcast('showProfileModal');
-                });
+                // $rootScope.$broadcast('queue-tutorial', 'main', false, function () {
+                //     // $rootScope.$broadcast('showProfileModal');
+                // });
 
             }
         }
